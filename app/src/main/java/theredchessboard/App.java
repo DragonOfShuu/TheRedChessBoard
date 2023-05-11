9e061404a36f7aad1e4ca4b51ec5ec7b6d51a3bb
Dragon of Shuu <68718280+MushroomEnder@users.noreply.github.com>
2023-05-11 10:57:54 -0600

    The Initial Red Commit

---
+++ app/src/main/java/theredchessboard/App.java
@@ -0,0 +1,11 @@
+package theredchessboard;
+
+public class App {
+    public String testExample() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().testExample());
+    }
+}
